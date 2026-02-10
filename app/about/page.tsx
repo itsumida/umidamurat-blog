f4f96c2ca3c547b0b217e6ae01369c315784d261
Umida Muratbekova <umidamurat@Mac.home>
2026-02-10 22:21:48 +0100

    Fix apostrophes in about page for build

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,13 +3,13 @@ export default function About() {
     <div className="prose" style={{ maxWidth: '65ch' }}>
       <h2 style={{ fontSize: '1.73rem', marginBottom: '1.75rem' }}>about</h2>
       <p>
-        Hi there, it is Umida here. I'm proud uzbek, currently in my last semester of bachelors in Oslo. I work at a startup company and I love my work and people I work with. 
+        Hi there, it is Umida here. I&apos;m proud uzbek, currently in my last semester of bachelors in Oslo. I work at a startup company and I love my work and people I work with.
       </p>
       <p>
-        I love reading, learning about how things work, and also writing. That's why I built this place - to share my thoughts. I'd love to meet awesome people and make awesome friends. In fact, this is one of the reasons I'm excited to have this blog.
+        I love reading, learning about how things work, and also writing. That&apos;s why I built this place - to share my thoughts. I&apos;d love to meet awesome people and make awesome friends. In fact, this is one of the reasons I&apos;m excited to have this blog.
       </p>
       <p>
-        If anything here resonates, reach out at to me at umidaamurat [at] gmail [dot] com. I'd love to hear from you :)
+        If anything here resonates, reach out at to me at umidaamurat [at] gmail [dot] com. I&apos;d love to hear from you :)
       </p>
     </div>
   );
